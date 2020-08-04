762edb87d81590d16ef448bddc5b56e4172d7a48
piotrs <piotrek.priv@gmail.com>
2020-08-04 18:46:29 +0200

    code cleaning

--- src/transitcalculator/TransitCalculator.java
+++ src/transitcalculator/TransitCalculator.java
@@ -1,29 +1,26 @@
-package TransitCalculator;
+package transitcalculator;
 
 import java.util.Arrays;
 
 public class TransitCalculator {
 
-    int numberOfDays;
-    int numberOfExpectedRides;
+    final int numberOfDays;
+    final int numberOfExpectedRides;
 
     public TransitCalculator(int numberOfDays, int numberOfExpectedRides) {
         this.numberOfDays = numberOfDays;
         this.numberOfExpectedRides = numberOfExpectedRides;
     }
 
-    double payPerRideFee = 2.75;
-    double unlimited7Fee = 33.0;
-    double unlimited30Fee = 127.0;
+    final double payPerRideFee = 2.75;
+    final double unlimited7Fee = 33.0;
+    final double unlimited30Fee = 127.0;
 
     public double unlimited7Price() {
 
-        int numberOfU7;
-
-        numberOfU7 = (numberOfDays % 7 == 0)? numberOfDays / 7 : numberOfDays / 7 + 1;
+        int numberOfU7 = (numberOfDays % 7 == 0) ? numberOfDays / 7 : numberOfDays / 7 + 1;
         return numberOfU7 * unlimited7Fee / numberOfExpectedRides;
     }
-
 
     public double[] getRidePrices() {
 
@@ -40,16 +37,16 @@ public class TransitCalculator {
 
         double[] priceOptionsPerRide = getRidePrices();
         double minFare = priceOptionsPerRide[0];
-        int minIndex = 0;
+        int minFareIndex = 0;
         for(int i = 1;  i < priceOptionsPerRide.length; i++) {
             if (priceOptionsPerRide[i] < minFare) {
                 minFare = priceOptionsPerRide[i];
-                minIndex = i;
+                minFareIndex = i;
             }
         }
-        if (minIndex == 0) {
+        if (minFareIndex == 0) {
             return "You should get the Pay-per-ride option at " + minFare + " per ride.";
-        } else if (minIndex == 1) {
+        } else if (minFareIndex == 1) {
             return "You should get the 7-day Unlimited option at " + minFare + " per ride.";
         } else {
             return "You should get the 30-day Unlimited option at " + minFare + " per ride.";
@@ -58,7 +55,7 @@ public class TransitCalculator {
 
     public static void main(String[] args) {
 
-        TransitCalculator t1 = new TransitCalculator(22, 3);
+        TransitCalculator t1 = new TransitCalculator(19, 29);
         System.out.println(Arrays.toString(t1.getRidePrices()));
         System.out.println(t1.getBestFare());
 
